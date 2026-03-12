cde12f38e8d4e7b06ba0307b9e936429911adb92
zykev <zeyu.chen07@gmail.com>
2026-03-12 18:05:58 +0800

    add visual mae result

--- scripts/pretrain.sh
+++ scripts/pretrain.sh
@@ -6,15 +6,15 @@ export CUDA_VISIBLE_DEVICES=0
 
 # 执行 Python 程序
 python main_pretrain.py \
-    --accum_iter 2 \
+    --accum_iter 4 \
     --batch_size 128 \
     --model mae_vit_base_patch16 \
     --resume ".checkpoints/intraoral/mae_pretrain_vit_base_full.pth" \
     --epochs 400 \
-    --warmup_epochs 20 \
-    --norm_pix_loss \
-    --mask_ratio 0.75 \
+    --warmup_epochs 40 \
+    --mask_ratio 0.1 \
     --blr 5e-4 \
     --weight_decay 0.05 \
     --data_path ".datasets/intraoral/intraoral" \
-    --output_dir "exp/pretrain_v1"
+    --output_dir "exp/pretrain_v2" \
+    --log_dir "exp/pretrain_v2"
